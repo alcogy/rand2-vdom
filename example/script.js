4bdbfa98228d4929585643c802fad38cf5c6f083
nasm <info@alcogy.com>
2025-05-14 14:14:26 +0900

    Update name.

--- example/script.js
+++ example/script.js
@@ -5,15 +5,15 @@ window.onload = async () => {
 }
 
 function createVDom(root, title) {
-  const whistler = new Whistler(root);
-  const [state, setState] = whistler.useState('counter', 0);
+  const vdom = new VDom(root);
+  const [state, setState] = vdom.useState('counter', 0);
   
   const elements = () => 
-    whistler.e('div', { classList: 'wrap' }, 
-      whistler.e('h1', null, title),
-      whistler.e('p', null, `count is ${state()}`),
-      whistler.e('button', { onclick: () => setState(state() + 1)}, 'push me'),
+    vdom.e('div', { classList: 'wrap' }, 
+      vdom.e('h1', null, title),
+      vdom.e('p', null, `count is ${state()}`),
+      vdom.e('button', { onclick: () => setState(state() + 1)}, 'push me'),
   );
 
-  whistler.mount(elements);
+  vdom.mount(elements);
 }
--- src/rand2-vdom.js
+++ src/rand2-vdom.js
@@ -1,4 +1,4 @@
-class Whistler {
+class VDom {
   constructor(root) {
     this.root = root;
     this.app = {};
